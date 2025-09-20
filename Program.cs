bd3fd9eb5e36edb80c6a4cbaf955a0577bb847ae
김동우 <hide4321@naver.com>
2025-09-21 03:21:04 +0900

    주석 사용 데이터 타입

--- Program.cs
+++ Program.cs
@@ -2,8 +2,17 @@
 {
     internal class Program
     {
+        // C#의 주석
+        /*
+         다중 주석
+         */
         static void Main(string[] args)
         {
+            // int     정수형 (-1 0 1 2 3 4)
+            // float   실수형 (1.0 3.14)
+            // string  문자열 ("Rookiss")
+            // bool    불리언 (true/false)
+
             Console.WriteLine("Hello, World!");
         }
     }
