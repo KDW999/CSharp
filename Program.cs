df27ce137bba42494b489df204eb23866d8aeee9
김동우 <hide4321@naver.com>
2025-10-29 20:17:25 +0900

    Debugging / Select Class

--- Program.cs
+++ Program.cs
@@ -4,22 +4,39 @@ using System.Threading.Channels;
 
 namespace CSharp
 {
-    internal class Program
-    {
-        static int Factorial(int n)
-        {
-            int sum = n;
-            for (int i=n-1; i>0; i--) 
-            {
-                sum = sum * i;
-            }
-            return sum;
-        }
-        static void Main(string[] args)
-        {
+    // 메소드 호출 => Inception
+    // 현실 -> 1차 꿈 -> 2차 꿈 -> 3차 꿈
+   //internal class Program
+   // {
+   //     static void Start()
+   //     {
+   //         Console.WriteLine("직업을 선택하세요!");
+   //         Console.WriteLine("[1] 기사");
+   //         Console.WriteLine("[2] 궁수");
+   //         Console.WriteLine("[3] 법사");
 
-            int ret = Factorial(5);
-            Console.WriteLine(ret);
-        }
-    }
+   //         int selClass = int.Parse(Console.ReadLine());
+
+   //         switch (selClass)
+   //         {
+   //             case 1:
+   //                 Console.WriteLine("기사 선택");
+   //                 break;
+   //             case 2:
+   //                 Console.WriteLine("궁수 선택");
+   //                 break;
+   //             case 3:
+   //                 Console.WriteLine("법사 선택");
+   //                 break;
+   //             default:
+   //                 Start();
+   //                 break;
+   //         }
+
+   //     }
+   //     static void Main(string[] args)
+   //     {
+   //         Program.Start();
+   //     }
+   // }
 }
