011686beec37ddf82ece401e789ff28e8d4950ed
김동우 <hide4321@naver.com>
2025-11-16 22:49:29 +0900

    Inheritance

--- Program.cs
+++ Program.cs
@@ -5,59 +5,15 @@ using System.Threading.Channels;
 
 namespace CSharp
 {
-    // 객체(OOP Object Oriented Programming)
-
-    // Ref 참조
-    class Knight
+    // OOP(은닉성/상속성/다형성)
+    class Player // 부모 클래스 OR 기반 클래스
     {
         // 필드
         static public int counter = 1; // 오로지 1개만 존재
-
         public int id;
         public int hp;
         public int attack;
 
-        static public void Test()
-        {
-            counter++;
-        }
-
-        static public Knight CreateKnight()
-        {
-            Knight knight = new Knight();
-            knight.hp = 100;
-            knight.attack = 10;
-            return knight;
-        }
-        public Knight()
-        {
-            id = counter;
-            counter++;
-
-            hp = 100;
-            attack = 0;
-            Console.WriteLine("생성자 호출");
-        }
-
-        public Knight(int hp) : this()
-        {
-            this.hp = hp;
-            Console.WriteLine("int 생성자 호출");
-        }
-
-        public Knight(int hp, int attack)
-        {
-            this.hp = hp;
-            this.attack = attack;
-            Console.WriteLine("int, int 생성자 호출");
-        }
-        public Knight Clone()
-        {
-            Knight knight = new Knight();
-            knight.hp = hp;
-            knight.attack = attack;
-            return knight;
-        }
         public void Move()
         {
             Console.WriteLine("knight Move");
@@ -68,13 +24,31 @@ namespace CSharp
         }
     }
 
+    class Mage : Player
+    {
+
+    }
+
+    class Archer : Player
+    {
+
+    }
+    class Knight : Player // 자식 OR 파생 클래스
+    {
+        public void Stun()
+        {
+
+        }
+        
+    }
+
     internal class Program
     {
 
         static void Main(string[] args)
         {
-            Knight knight = Knight.CreateKnight(); // static
-            knight.Move(); // 일반
+            Knight knight = new Knight();
+            knight.Move();
         }
     }
 }
