b9a8ed0fa17c3313f4ace8c457ce01e08ce247d6
김동우 <hide4321@naver.com>
2025-11-17 23:14:48 +0900

    Encapsulation

--- Program.cs
+++ Program.cs
@@ -6,49 +6,41 @@ using System.Threading.Channels;
 namespace CSharp
 {
     // OOP(은닉성/상속성/다형성)
-    class Player // 부모 클래스 OR 기반 클래스
-    {
-        // 필드
-        static public int counter = 1; // 오로지 1개만 존재
-        public int id;
-        public int hp;
-        public int attack;
+   
+    // 자동차
+    // 핸들 페달 차문
+    // 전기장치 엔진 기름 <-> 외부 노출
+    // 설계 시 위험하냐, 대충 사용해도되냐의 보안레벨
 
-        public void Move()
-        {
-            Console.WriteLine("knight Move");
-        }
-        public void Attack()
-        {
-            Console.WriteLine("knight Attack");
-        }
-    }
-
-    class Mage : Player
+    class Knight
     {
+        // 접근 한정자
+        // public 모두 접근
+        // protected 자식 클래스는 접근 가능
+        // private 같은 클래스 내에서만, 접근한정자 아무것도 안붙이면 기본적으로 private
+        // 변수를 private 메서드를 public으로 사용하면 여러 곳에서 hp를 사용 시 어디서 사용했는지 찾기가 쉽다
+        protected int hp;
+ 
 
     }
 
-    class Archer : Player
+    class SuperKnight : Knight
     {
-
-    }
-    class Knight : Player // 자식 OR 파생 클래스
-    {
-        public void Stun()
+        void Test()
         {
-
+            hp = 10;
         }
+    }
+
         
-    }
 
     internal class Program
     {
-
         static void Main(string[] args)
         {
             Knight knight = new Knight();
-            knight.Move();
+            knight.SetHp(100);
+
         }
     }
 }
