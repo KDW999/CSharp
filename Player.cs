997c7384dc3d093269558bf70bedcb58f650ce8a
김동우 <hide4321@naver.com>
2025-11-21 23:45:35 +0900

    Create Player Info

---
+++ Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    enum PlayerType
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+    class Player
+    {
+        protected PlayerType type = PlayerType.None;
+        protected int hp = 0;
+        protected int attack = 0;
+
+        protected Player(PlayerType type)
+        {
+            this.type = type;
+        }
+
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public PlayerType GetPlayerType() { return type; }
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+        public bool IsDead() { return hp <= 0; }
+        public void OnDamaged( int damage)
+        {
+            hp -= damage;
+            if (hp <= 0) hp = 0;
+        }
+        
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Knight)
+        {
+            SetInfo(75, 12);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Knight)
+        {
+            SetInfo(65, 15);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,24 +8,9 @@ namespace CSharp
 
     internal class Program
     {
-      
         static void Main(string[] args)
         {
-            string name = "Harry Potter";
-
-            // 1. 찾기
-            bool found = name.Contains("Harry");
-            int index = name.IndexOf('P');
-
-            // 2. 변형
-            name = name + " Junior";
-            string lowerCaseName = name.ToLower();
-            string upperrCaseName = name.ToUpper();
-            string newName = name.Replace("r", "l");
-
-            // 3. 분할
-            string[] names = name.Split(new char[] { ' ' });
-            string substringName = name.Substring(5);
+            Player player = new Knight();
         }
     }
 }
