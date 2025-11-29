e32a9e747ca8c314ad80c7a43bf948cb79cbd5bc
김동우 <hide4321@naver.com>
2025-11-29 18:27:52 +0900

    Event , Delegate

---
+++ InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    // Observer Parttern : 특정 상황 발생 시 알려주는 디자인 패턴
+    internal class InputManager
+    {
+        public delegate void OnInputKey();
+        public event OnInputKey InputKey;
+
+        public void Update()
+        {
+            if (Console.KeyAvailable == false)
+                return;
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            if (info.Key == ConsoleKey.A) // A키를 누른다면
+            {
+                // 모두에게 알려주기
+                InputKey();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,39 +8,22 @@ namespace CSharp
 
     internal class Program
     {
-        // delegate -> 함수 자체를 인자로 넘겨주는 형식
-        // 반환:int, 입력:void
-        // OnClicked이 delegate 형식의 이름
-        delegate int OnClicked();
-
-        static void ButtonPressed(OnClicked clickedFunction)
+        static void OnInputTest()
         {
-            // 함수 호출
-            clickedFunction();
-        }
-
-        static int TestDelegate()
-        {
-            Console.WriteLine("Hello Delegate");
-            return 0;
-        }
-
-        static int TestDelegate2()
-        {
-            Console.WriteLine("Hello Delegate 2");
-            return 0;
-        }
+            Console.WriteLine("Input Received!");
+        }  
 
         static void Main(string[] args)
         {
-            // ★ delegate (대리자), CallBack
-            // 메서드를 데이터 취급하여 전달할 수 있고, 어떤 메서드가 실행될 지는 실행 시점에 정한다.
-            Console.WriteLine();
+             InputManager inputManager = new InputManager();
+             inputManager.InputKey += OnInputTest;
+            while (true)
+            {
+                inputManager.Update();
+            }
 
-            OnClicked clicked = new OnClicked(TestDelegate);
-            clicked += TestDelegate2; // 여러 함수를 체이닝해서 사용 가능
-
-            ButtonPressed(clicked);
+            //inputManager.InputKey(); Event는 Delegate와 다르게 이런 식으로 단독으로 호출이 불가
+            // 외부에서 멋대로 부르지 못하게 하기 위함
         }
     }
 }
