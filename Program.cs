7ae54a27387ac1e7705b0d34ab923b10d89de3db
김동우 <hide4321@naver.com>
2025-11-30 03:24:17 +0900

    Nullable

--- Program.cs
+++ Program.cs
@@ -9,49 +9,57 @@ namespace CSharp
 
     internal class Program
     {
-
-        // Attribute : 실행 시 주석을 볼 수 있게
-        class Important : System.Attribute
+        static int Find()
         {
-            string message;
-
-            public Important(string message) { this.message = message; }
+            return 0;
         }
+
         class Monster
         {
-            // hp입니다. 중요한 정보
-            [Important("Very Important")]
-            public int hp;
-            protected int attack;
-            private float speed;
-            void Attack() { }
+            public int Id { get; set; }
         }
         static void Main(string[] args)
         {
-            // Reflection : X-Ray, 동적으로 구현 시
-            Monster monster = new Monster();
-            Type type = monster.GetType();
+            // Nullable -> Null + able
+            int? number = null;
 
-           var fields = type.GetFields(System.Reflection.BindingFlags.Public
-                | System.Reflection.BindingFlags.NonPublic
-                | System.Reflection.BindingFlags.Static
-                | System.Reflection.BindingFlags.Instance);
+            // 이런 식으로 아래의 null 체크를 한 번에 가능
+            int b = number ?? 0; // 값이 있으면 원래값 쓰고 null이라면 0으로 초기화
+            Console.WriteLine(b);
 
-            foreach (FieldInfo field in fields)
+            //int c = (number != null) ? number.Value : 0; 삼항연산자랑 유사하다
+
+            if(number != null)
             {
-                string access = "protected";
-                if (field.IsPublic) 
-                    access = "public";
-                else if (field.IsPrivate)
-                    access = "private";
+                int a = number.Value;
+                Console.WriteLine(a);
+            }
 
-                var attributes = field.GetCustomAttributes();
+            if (number.HasValue)
+            {
+                int a = number.Value;
+                Console.WriteLine(a);
+            }
 
-                // 데이터의 타입을 볼 수 있게
-                Console.WriteLine($"{access} {field.FieldType.Name} {field.Name}");
-                // => public Int32 hp
-                // protected Int32 attack
-                // private Single speed
+            // Nullable -> ?를 붙이는 형식
+            // null을 가질 수 있음
+            // ?? / ?. 처럼 null 체크를 줄여서 사용 가능
+            Monster monster = null;
+
+            if(monster != null)
+            {
+                int monsterId = monster.Id;
+            }
+
+            int? id = monster?.Id; // monster가 null이라면 id는 null 값이 있다면 id = monster.Id
+            // 위의 형식은 아래의 조건 체크와 같다
+            if(monster == null)
+            {
+                id = null;
+            }
+            else
+            {
+                id = monster.Id;
             }
         }
     }
