0281fcc455677a54240962f1f5a222b0756c88b3
김동우 <hide4321@naver.com>
2025-11-30 02:57:59 +0900

    Reflection, Attribute

--- Program.cs
+++ Program.cs
@@ -1,42 +1,57 @@
 ﻿using System.Collections.Specialized;
 using System.ComponentModel.Design;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading.Channels;
 
 namespace CSharp
 {
-    
+
     internal class Program
     {
-        class TestException : Exception
+
+        // Attribute : 실행 시 주석을 볼 수 있게
+        class Important : System.Attribute
         {
+            string message;
 
+            public Important(string message) { this.message = message; }
         }
-
+        class Monster
+        {
+            // hp입니다. 중요한 정보
+            [Important("Very Important")]
+            public int hp;
+            protected int attack;
+            private float speed;
+            void Attack() { }
+        }
         static void Main(string[] args)
         {
-            try
-            {
-                // 1. 0으로 나눌 때
-                // 2. 잘못된 메모리 참조 (null)
-                // 3. 오버플로우
-                int a = 10;
-                int b = 0;
-                int result = a / b;
+            // Reflection : X-Ray, 동적으로 구현 시
+            Monster monster = new Monster();
+            Type type = monster.GetType();
 
-                //throw new TestException();
-            }
-            catch (DivideByZeroException)
-            {
+           var fields = type.GetFields(System.Reflection.BindingFlags.Public
+                | System.Reflection.BindingFlags.NonPublic
+                | System.Reflection.BindingFlags.Static
+                | System.Reflection.BindingFlags.Instance);
 
-            }
-            catch (Exception e)
+            foreach (FieldInfo field in fields)
             {
-                Console.WriteLine(e);
-            }
-            finally
-            {
-                // DB, 파일 정리 등
+                string access = "protected";
+                if (field.IsPublic) 
+                    access = "public";
+                else if (field.IsPrivate)
+                    access = "private";
+
+                var attributes = field.GetCustomAttributes();
+
+                // 데이터의 타입을 볼 수 있게
+                Console.WriteLine($"{access} {field.FieldType.Name} {field.Name}");
+                // => public Int32 hp
+                // protected Int32 attack
+                // private Single speed
             }
         }
     }
