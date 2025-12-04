495a08a16bbf224c03cfa9a96135add567d9d5a7
김동우 <hide4321@naver.com>
2025-12-04 22:33:04 +0900

    Setting Map

--- Algorithm/Board.cs
+++ Algorithm/Board.cs
@@ -6,53 +6,64 @@ using System.Threading.Tasks;
 
 namespace CSharp.Algorithm
 {
-    class MyList<T>
-    {
-        const int DEFAULT_SIZE = 1;
-        T[] _data = new T[DEFAULT_SIZE];
 
-        public int Count; // 실제로 사용 중인 데이터 갯수
-        public int Capacity { get { return _data.Length; } } // 예약된 데이터 갯수
-
-        // 0(1) 예외 케이스 : 이사 비용 무시
-        public void Add(T item)
-        {
-            // 1. 공간이 충분히 남아 있는지 확인
-            if (Count >= Capacity)
-            {
-                // 공간을 다시 늘려서 확보
-                T[] newArray = new T[Count * 2];
-                for (int i = 0; i < Count; i++)
-                    newArray[i] = _data[i];
-                _data = newArray;
-            }
-            // 2. 공간에 데이터를 넣어준다.
-            _data[Count] = item;
-            Count++;
-        }
-
-        // 0(1)
-
-        public T this[int index]
-        {
-            get { return _data[index]; }
-            set { _data[index] = value; }
-        }
-
-        // O(N)
-        public void RemoveAt(int index)
-        {
-            for (int i = index; i < Count - 1; i++)
-                _data[i] = _data[i + 1];
-
-            _data[Count - 1] = default(T);
-            Count--;
-        }
-    }
 
     internal class Board
     {
-        public int[] _data = new int[25];
+        const char CIRCLE = '\u25cf';
+        public TileType[,] _tile; // 배열
+        public int _size;
 
+        public enum TileType
+        {
+            Empty,
+            Wall,
+        }
+        public void Initialize(int size)
+        {
+            _tile = new TileType[size, size];
+            _size = size;
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x == 0 || x == _size - 1 || y == 0 || y == size - 1)
+                        _tile[y, x] = TileType.Wall;
+                    else
+                        _tile[y, x] = TileType.Empty;
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = prevColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
+
+        }
     }
 }
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -15,12 +15,12 @@ namespace CSharp
         {
 
             Board board = new Board();
-            board.Initialize();
+            board.Initialize(25);
 
             Console.CursorVisible = false;
 
             const int WAIT_TICK = 1000 / 30;
-            const char CIRCLE = '\u25cf';
+            
 
             int lastTick = 0;
             while (true)
@@ -39,16 +39,7 @@ namespace CSharp
 
                 // 렌더링
                 Console.SetCursorPosition(0, 0);
-                
-                for(int i=0; i<25; i++)
-                {
-                    for(int j=0; j<25; j++)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.Write(CIRCLE);
-                    }
-                    Console.WriteLine();
-                }
+                board.Render();
             }
         }
     }
