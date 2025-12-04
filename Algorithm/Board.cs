b5aed18e0b7cd4ea27d8c65e7ca4d0d4ebf09639
김동우 <hide4321@naver.com>
2025-12-04 22:13:31 +0900

    Dynamic Array (List) Modify

--- Algorithm/Board.cs
+++ Algorithm/Board.cs
@@ -6,60 +6,46 @@ using System.Threading.Tasks;
 
 namespace CSharp.Algorithm
 {
-    class MyLinkedListNode<T>
+    class MyList<T>
     {
-        public T Data;
-        public MyLinkedListNode<T> Next;
-        public MyLinkedListNode<T> Prev;
-    }
+        const int DEFAULT_SIZE = 1;
+        T[] _data = new T[DEFAULT_SIZE];
 
-    class MyLinkedList<T>
-    {
-        public MyLinkedListNode<T> Head = null; // 첫 번째
-        public MyLinkedListNode<T> Tail = null; // 마지막
-        public int Count = 0;
+        public int Count; // 실제로 사용 중인 데이터 갯수
+        public int Capacity { get { return _data.Length; } } // 예약된 데이터 갯수
 
-        // 0(1)
-        public MyLinkedListNode<T> AddLast(T data)
+        // 0(1) 예외 케이스 : 이사 비용 무시
+        public void Add(T item)
         {
-            MyLinkedListNode<T> newRoom = new MyLinkedListNode<T>();
-            newRoom.Data = data;
-
-            // 아직 방이 아예 없었다면, 새로 추가한 첫 번째 방이 곧 Head
-            if (Head == null)
-                Head = newRoom;
-
-            // 기존의 마지막 방과 새로 추가되는 방을 연결
-            if (Tail != null)
+            // 1. 공간이 충분히 남아 있는지 확인
+            if (Count >= Capacity)
             {
-                Tail.Next = newRoom;
-                newRoom.Prev = Tail;
+                // 공간을 다시 늘려서 확보
+                T[] newArray = new T[Count * 2];
+                for (int i = 0; i < Count; i++)
+                    newArray[i] = _data[i];
+                _data = newArray;
             }
-
-            // 새로 추가되는 방을 마지막 방으로 인정
-            Tail = newRoom;
+            // 2. 공간에 데이터를 넣어준다.
+            _data[Count] = item;
             Count++;
-            return newRoom;
         }
 
         // 0(1)
-        public void Remove(MyLinkedListNode<T> room)
+
+        public T this[int index]
         {
-            // 삭제하는 게 첫 번째 방이라면 두 번째 방이 첫 번째 방이 된다
-            if (Head == room)
-                Head = Head.Next;
+            get { return _data[index]; }
+            set { _data[index] = value; }
+        }
 
-            // 삭제하는 게 마지막 방이라면 마지막 앞 방이 마지막 방이 된다
-            if (Tail == room)
-                Tail = Tail.Prev;
+        // O(N)
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+                _data[i] = _data[i + 1];
 
-            // 삭제할 노드의 앞 뒤 연결
-            if (room.Prev != null)
-                room.Prev.Next = room.Next;
-
-            if (room.Next != null)
-                room.Next.Prev = room.Prev;
-
+            _data[Count - 1] = default(T);
             Count--;
         }
     }
@@ -67,16 +53,6 @@ namespace CSharp.Algorithm
     internal class Board
     {
         public int[] _data = new int[25];
-        public MyLinkedList<int> _data3 = new MyLinkedList<int>();
-        public void Initialize()
-        {
-            _data3.AddLast(101);
-            _data3.AddLast(102);
-            MyLinkedListNode<int> node = _data3.AddLast(103);
-            _data3.AddLast(104);
-            _data3.AddLast(105);
 
-            _data3.Remove(node);
-        }
     }
 }
