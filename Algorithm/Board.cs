fe162011d81c59f135d82b4a29ce7cc9a820d3e3
김동우 <hide4321@naver.com>
2025-12-06 22:29:36 +0900

    SideWinder

--- Algorithm/Board.cs
+++ Algorithm/Board.cs
@@ -26,12 +26,10 @@ namespace CSharp.Algorithm
             _tile = new TileType[size, size];
             _size = size;
 
-            GenerateByBinaryTree();
-             
+            //GenerateByBinaryTree();
+            GenerateBySideWinder();
         }
-
-        // 짝수 인덱스에서 랜덤 방향으로 벽을 뚫고 나가서 출구를 찾는 미로 알고리즘 느낌
-        void GenerateByBinaryTree()
+        void GenerateBySideWinder()
         {
             // 길 막는 작업
             for (int y = 0; y < _size; y++)
@@ -45,11 +43,10 @@ namespace CSharp.Algorithm
                 }
             }
 
-            // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
-            // Binary Tree Algorithm
             Random rand = new Random();
             for (int y = 0; y < _size; y++)
             {
+                int count = 1;
                 for (int x = 0; x < _size; x++)
                 {
                     if (x % 2 == 0 || y % 2 == 0)
@@ -73,10 +70,68 @@ namespace CSharp.Algorithm
                     if (rand.Next(0, 2) == 0)
                     {
                         _tile[y, x + 1] = TileType.Empty;
+                        count++;
                     }
                     else
                     {
-                        _tile[y + 1, x] = TileType.Empty;
+                        int randomIndex = rand.Next(0, count);
+                        _tile[y + 1, x - randomIndex * 2] = TileType.Empty;
+                        count = 1;
+                    }
+                }
+
+
+            }
+        }
+        // 짝수 인덱스에서 랜덤 방향으로 벽을 뚫고 나가서 출구를 찾는 미로 알고리즘 느낌
+        void GenerateByBinaryTree()
+        {
+            {
+                // 길 막는 작업
+                for (int y = 0; y < _size; y++)
+                {
+                    for (int x = 0; x < _size; x++)
+                    {
+                        if (x % 2 == 0 || y % 2 == 0)
+                            _tile[y, x] = TileType.Wall;
+                        else
+                            _tile[y, x] = TileType.Empty;
+                    }
+                }
+
+                // 랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+                // Binary Tree Algorithm
+                Random rand = new Random();
+                for (int y = 0; y < _size; y++)
+                {
+                    for (int x = 0; x < _size; x++)
+                    {
+                        if (x % 2 == 0 || y % 2 == 0)
+                            continue;
+
+                        if (y == _size - 2 && x == _size - 2)
+                            continue;
+
+                        if (y == _size - 2)
+                        {
+                            _tile[y, x + 1] = TileType.Empty;
+                            continue;
+                        }
+
+                        if (x == _size - 2)
+                        {
+                            _tile[y + 1, x] = TileType.Empty;
+                            continue;
+                        }
+
+                        if (rand.Next(0, 2) == 0)
+                        {
+                            _tile[y, x + 1] = TileType.Empty;
+                        }
+                        else
+                        {
+                            _tile[y + 1, x] = TileType.Empty;
+                        }
                     }
                 }
             }
