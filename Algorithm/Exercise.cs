62c88d291f5f140950ed4314b75567f5c33505a8
김동우 <hide4321@naver.com>
2025-12-10 18:41:00 +0900

    Stack, Queue

---
+++ Algorithm/Exercise.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Algorithm
+{
+    internal class Exercise
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(101);
+            stack.Push(102);
+            stack.Push(103);
+            stack.Push(104);
+            stack.Push(105);
+            int data = stack.Pop();
+            int data2 = stack.Peek();
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(101);
+            queue.Enqueue(102);
+            queue.Enqueue(103);
+            queue.Enqueue(104);
+            queue.Enqueue(105);
+
+            int data3 = queue.Dequeue();
+            int data4 = queue.Peek();
+
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(101);
+            list.AddLast(102);
+            list.AddLast(103);
+
+            // FIFO
+            int value1 = list.First.Value;
+            list.RemoveFirst();
+
+            // LIFO
+            int value2 = list.Last.Value;
+            list.RemoveLast();
+            Console.WriteLine($"value1: {value1} / value2: {value2}");
+        }
+    }
+}
