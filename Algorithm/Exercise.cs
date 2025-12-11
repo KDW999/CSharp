86185a8008811ce7b3c0ba3e37b8bf2ed2e82ac5
김동우 <hide4321@naver.com>
2025-12-11 18:28:25 +0900

    Create Graph

--- Algorithm/Exercise.cs
+++ Algorithm/Exercise.cs
@@ -1,48 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharp.Algorithm
+﻿namespace CSharp.Algorithm
 {
+    class Graph
+    {
+        int[,] adj = new int[6, 6]
+        {
+            {0,1,0,1,0,0},
+            {1,0,1,1,0,0},
+            {0,1,0,0,0,0},
+            {1,1,0,0,1,0},
+            {0,0,0,1,0,1},
+            {0,0,0,0,1,0}
+        };
+
+        List<int>[] adj2 = new List<int>[]
+        {
+            new List<int>(){1,3},
+            new List<int>(){0,2,3},
+            new List<int>(){1},
+            new List<int>(){0,1,4},
+            new List<int>(){3,5},
+            new List<int>(){4},
+        };
+    }
     internal class Exercise
     {
         static void Main(string[] args)
         {
-            Stack<int> stack = new Stack<int>();
+            // DFS (Depth First Search 깊이 우선 탐색)
+            // BFS (Breadth First Search 너비 우선 탐색)
 
-            stack.Push(101);
-            stack.Push(102);
-            stack.Push(103);
-            stack.Push(104);
-            stack.Push(105);
-            int data = stack.Pop();
-            int data2 = stack.Peek();
-
-            Queue<int> queue = new Queue<int>();
-            queue.Enqueue(101);
-            queue.Enqueue(102);
-            queue.Enqueue(103);
-            queue.Enqueue(104);
-            queue.Enqueue(105);
-
-            int data3 = queue.Dequeue();
-            int data4 = queue.Peek();
-
-            LinkedList<int> list = new LinkedList<int>();
-            list.AddLast(101);
-            list.AddLast(102);
-            list.AddLast(103);
-
-            // FIFO
-            int value1 = list.First.Value;
-            list.RemoveFirst();
-
-            // LIFO
-            int value2 = list.Last.Value;
-            list.RemoveLast();
-            Console.WriteLine($"value1: {value1} / value2: {value2}");
         }
     }
 }
