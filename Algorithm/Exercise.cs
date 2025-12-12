3ecd68dc023be7e5b2a4a81e8e06b33074fa9576
김동우 <hide4321@naver.com>
2025-12-12 20:14:33 +0900

    BFS

--- Algorithm/Exercise.cs
+++ Algorithm/Exercise.cs
@@ -7,8 +7,8 @@
             {0,1,0,1,0,0},
             {1,0,1,1,0,0},
             {0,1,0,0,0,0},
-            {1,1,0,0,0,0},
-            {0,0,0,0,0,1},
+            {1,1,0,0,1,0},
+            {0,0,0,1,0,1},
             {0,0,0,0,1,0}
         };
 
@@ -17,54 +17,42 @@
             new List<int>(){1,3},
             new List<int>(){0,2,3},
             new List<int>(){1},
-            new List<int>(){0,1},
-            new List<int>(){5},
+            new List<int>(){0,1,4},
+            new List<int>(){3,5},
             new List<int>(){4},
         };
 
-        bool[] visited = new bool[6];
-        // 1) now부터 방문
-        // 2) now와 연결된 정점들을 하나씩 확인, 미방문 상태만 방문
-
-        // 2차원 행렬
-        public void DFS(int now)
+        public void BFS(int start)
         {
-            Console.WriteLine(now);
-            visited[now] = true; // 1) now 방문
+            bool[] found = new bool[6];
+            int[] parent = new int[6];
+            int[] distance = new int[6];
 
-            for(int next = 0; next<6; next++)
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+            parent[start] = start;
+            distance[start] = 0;
+
+            while (q.Count > 0)
             {
-                if (adj[now, next] == 0) // 연결되어 있지 않으면 스킵
-                    continue;
-                if (visited[next]) // 이미 방문했으면 스킵
-                    continue;
+                int now = q.Dequeue();
+                Console.WriteLine(now);
 
-                DFS(next);
+                for(int next = 0; next<6; next++)
+                {
+                    if (adj[now, next] == 0) // 인정하지 않으면 스킵
+                        continue;
+                    if (found[next]) // 이미 발견했으면 스킵
+                        continue;
+                    q.Enqueue(next);
+                    found[next] = true;
+                    parent[next] = now;
+                    distance[next] = distance[now] + 1;
+                }
             }
         }
-
-    // 인접 리스트
-        public void DFS2(int now)
-        {
-            Console.WriteLine(now);
-            visited[now] = true;
-
-            foreach(int next in adj2[now])
-            {
-                if (visited[next])
-                    continue;
-                DFS2(next);
-            }
-        }
-
-        // 간선이 끊어진 경우 모두 탐색
-        public void SearchAll()
-        {
-            visited = new bool[6];
-            for (int now = 0; now < 6; now++)
-                if (visited[now] == false)
-                    DFS(now);
-        }
+         
     }
 
     internal class Exercise
@@ -74,9 +62,7 @@
             // DFS (Depth First Search 깊이 우선 탐색)
             // BFS (Breadth First Search 너비 우선 탐색)
             Graph graph = new Graph();
-            //graph.DFS(3);
-            //graph.DFS2(0);
-            graph.SearchAll();
+            graph.BFS(0);
         }
     }
 }
