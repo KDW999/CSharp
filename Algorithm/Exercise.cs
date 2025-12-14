adcc0c5316f824f01e9cefec7bb15d8850a002f3
김동우 <hide4321@naver.com>
2025-12-14 23:52:24 +0900

    Djikstra

--- Algorithm/Exercise.cs
+++ Algorithm/Exercise.cs
@@ -1,68 +1,122 @@
-﻿//namespace CSharp.Algorithm
-//{
-//    class Graph
-//    {
-//        int[,] adj = new int[6, 6]
-//        {
-//            {0,1,0,1,0,0},
-//            {1,0,1,1,0,0},
-//            {0,1,0,0,0,0},
-//            {1,1,0,0,1,0},
-//            {0,0,0,1,0,1},
-//            {0,0,0,0,1,0}
-//        };
+﻿namespace CSharp.Algorithm
+{
+    class Graph
+    {
+        int[,] adj = new int[6, 6]
+        {
+            {-1,15,-1,35,-1,-1},
+            {15,-1,05,10,-1,-1},
+            {-1,05,-1,-1,-1,-1},
+            {35,10,-1,-1,05,-1},
+            {-1,-1,-1,05,-1,05},
+            {-1,-1,-1,-1,05,-1}
+        };
 
-//        List<int>[] adj2 = new List<int>[]
-//        {
-//            new List<int>(){1,3},
-//            new List<int>(){0,2,3},
-//            new List<int>(){1},
-//            new List<int>(){0,1,4},
-//            new List<int>(){3,5},
-//            new List<int>(){4},
-//        };
+        public void Dijikstra(int start)
+        {
+            bool[] visited = new bool[6];
+            int[] distance = new int[6];
+            int[] parent = new int[6];
+            Array.Fill(distance, Int32.MaxValue);
 
-//        public void BFS(int start)
-//        {
-//            bool[] found = new bool[6];
-//            int[] parent = new int[6];
-//            int[] distance = new int[6];
+            distance[start] = 0;
+            parent[start] = start;
 
-//            Queue<int> q = new Queue<int>();
-//            q.Enqueue(start);
-//            found[start] = true;
-//            parent[start] = start;
-//            distance[start] = 0;
+            while (true)
+            {
+                // 제일 좋은 후보를 찾기
+                // 가장 유력 후보의 거리와 번호 저장
+                int closest = Int32.MaxValue;
+                int now = -1;
+                for (int i = 0; i < 6; i++)
+                {
+                    // 이미 방문한 정점 스킵
+                    if (visited[i])
+                        continue;
+                    // 아직 발견된 적이 없거나, 기존 후보보다 멀리 있으면 스킵
+                    if (distance[i] == Int32.MaxValue || distance[i] >= closest)
+                        continue;
 
-//            while (q.Count > 0)
-//            {
-//                int now = q.Dequeue();
-//                Console.WriteLine(now);
+                    // 가장 좋은 후보면 정보 갱신
+                    closest = distance[i];
+                    now = i;
+                }
 
-//                for(int next = 0; next<6; next++)
-//                {
-//                    if (adj[now, next] == 0) // 인정하지 않으면 스킵
-//                        continue;
-//                    if (found[next]) // 이미 발견했으면 스킵
-//                        continue;
-//                    q.Enqueue(next);
-//                    found[next] = true;
-//                    parent[next] = now;
-//                    distance[next] = distance[now] + 1;
-//                }
-//            }
-//        }
-         
-//    }
+                // 다음 후보가 하나도 없으면 종료
+                if (now == -1)
+                    break;
 
-//    internal class Exercise
-//    {
-//        static void Main(string[] args)
-//        {
-//            // DFS (Depth First Search 깊이 우선 탐색)
-//            // BFS (Breadth First Search 너비 우선 탐색)
-//            Graph graph = new Graph();
-//            graph.BFS(0);
-//        }
-//    }
-//}
+                // 제일 좋은 후보 찾았으니 방문
+                visited[now] = true;
+
+                // 방문한 정점과 인접한 정점들을 조사, 상황에 따라 발견한 최단거리 갱신
+                for (int next = 0; next < 6; next++)
+                {
+                    // 연결되지 않은 정점 스킵
+                    if (adj[now, next] == -1)
+                        continue;
+
+                    // 이미 방문한 정점은 스킵
+                    if (visited[next])
+                        continue;
+
+                    // 새로 조사된 정점의 최단거리 계산
+                    int nextDist = distance[now] + adj[now, next];
+
+                    // 기존 최단거리보다 작으면 갱신
+                    if (nextDist < distance[next])
+                    {
+                        distance[next] = nextDist;
+                        parent[next] = now;
+                    }
+                }
+            }
+
+            foreach(int num in distance){
+                Console.WriteLine(num);
+            }
+        }
+        public void BFS(int start)
+        {
+            bool[] found = new bool[6];
+            int[] parent = new int[6];
+            int[] distance = new int[6];
+
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(start);
+            found[start] = true;
+            parent[start] = start;
+            distance[start] = 0;
+
+            while (q.Count > 0)
+            {
+                int now = q.Dequeue();
+                Console.WriteLine(now);
+
+                for (int next = 0; next < 6; next++)
+                {
+                    if (adj[now, next] == 0) // 인정하지 않으면 스킵
+                        continue;
+                    if (found[next]) // 이미 발견했으면 스킵
+                        continue;
+                    q.Enqueue(next);
+                    found[next] = true;
+                    parent[next] = now;
+                    distance[next] = distance[now] + 1;
+                }
+            }
+        }
+
+    }
+
+    internal class Exercise
+    {
+        static void Main(string[] args)
+        {
+            // DFS (Depth First Search 깊이 우선 탐색)
+            // BFS (Breadth First Search 너비 우선 탐색)
+            Graph graph = new Graph();
+            graph.Dijikstra(0);
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,50 +1,50 @@
-﻿using CSharp.Algorithm;
-using System.Collections.Specialized;
-using System.ComponentModel.Design;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Threading.Channels;
+﻿//using CSharp.Algorithm;
+//using System.Collections.Specialized;
+//using System.ComponentModel.Design;
+//using System.Reflection;
+//using System.Runtime.CompilerServices;
+//using System.Threading.Channels;
 
-namespace CSharp
-{
+//namespace CSharp
+//{
 
-    class Program
-    {
+//    class Program
+//    {
 
-        static void Main(string[] args)
-        {
+//        static void Main(string[] args)
+//        {
 
-            Board board = new Board();
-            Player player = new Player();
-            board.Initialize(25, player);
-            player.Initialize(1, 1, board);
+//            Board board = new Board();
+//            Player player = new Player();
+//            board.Initialize(25, player);
+//            player.Initialize(1, 1, board);
 
-            Console.CursorVisible = false;
+//            Console.CursorVisible = false;
 
-            const int WAIT_TICK = 1000 / 30;
+//            const int WAIT_TICK = 1000 / 30;
 
-            int lastTick = 0;
-            while (true)
-            {
-                #region 프레임 관리
-                // 만약에 경과한 시간이 1/30초보다 작다면
-                int currentTick = System.Environment.TickCount;
+//            int lastTick = 0;
+//            while (true)
+//            {
+//                #region 프레임 관리
+//                // 만약에 경과한 시간이 1/30초보다 작다면
+//                int currentTick = System.Environment.TickCount;
 
-                if (currentTick - lastTick < WAIT_TICK)
-                    continue;
+//                if (currentTick - lastTick < WAIT_TICK)
+//                    continue;
 
-                int deltaiTick = currentTick - lastTick;
-                lastTick = currentTick;
-                #endregion
-                // 입력
+//                int deltaiTick = currentTick - lastTick;
+//                lastTick = currentTick;
+//                #endregion
+//                // 입력
 
-                // 로직
-                player.Update(deltaiTick);
+//                // 로직
+//                player.Update(deltaiTick);
 
-                // 렌더링
-                Console.SetCursorPosition(0, 0);
-                board.Render();
-            }
-        }
-    }
-}
+//                // 렌더링
+//                Console.SetCursorPosition(0, 0);
+//                board.Render();
+//            }
+//        }
+//    }
+//}
