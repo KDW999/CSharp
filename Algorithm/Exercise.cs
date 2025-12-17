2753b1e06042cfee7cf985d2f8dfa43020c6f9e9
김동우 <hide4321@naver.com>
2025-12-17 17:38:41 +0900

    A* Algorithm

--- Algorithm/Exercise.cs
+++ Algorithm/Exercise.cs
@@ -1,115 +1,115 @@
-﻿namespace CSharp.Algorithm
-{
-    class PriorityQueue<T> where T : IComparable<T>
-    {
-        List<T> _heap = new List<T>();
+﻿//namespace CSharp.Algorithm
+//{
+//    class PriorityQueue<T> where T : IComparable<T>
+//    {
+//        List<T> _heap = new List<T>();
 
-        // 0(logN)
-        public void Push(T data)
-        {
-            // 힙의 맨 끝에 새로운 데이터를 삽입한다
-            _heap.Add(data);
+//        // 0(logN)
+//        public void Push(T data)
+//        {
+//            // 힙의 맨 끝에 새로운 데이터를 삽입한다
+//            _heap.Add(data);
 
-            int now = _heap.Count - 1;
+//            int now = _heap.Count - 1;
 
-            // 도장깨기 시작
-            while (now > 0)
-            {
-                // 도장깨기 시도
-                int next = (now - 1) / 2;
-                if (_heap[now].CompareTo(_heap[next]) < 0)
-                    break; // 도장깨기 실패
+//            // 도장깨기 시작
+//            while (now > 0)
+//            {
+//                // 도장깨기 시도
+//                int next = (now - 1) / 2;
+//                if (_heap[now].CompareTo(_heap[next]) < 0)
+//                    break; // 도장깨기 실패
 
-                // 두 값 교체
-                T temp = _heap[now];
-                _heap[now] = _heap[next];
-                _heap[next] = temp;
+//                // 두 값 교체
+//                T temp = _heap[now];
+//                _heap[now] = _heap[next];
+//                _heap[next] = temp;
 
-                // 검사 위치 이동
-                now = next;
-            }
-        }
+//                // 검사 위치 이동
+//                now = next;
+//            }
+//        }
 
-        // 0(logN)
-        public T Pop()
-        {
-            // 반환할 데이터를 따로 저장
-            T ret = _heap[0];
+//        // 0(logN)
+//        public T Pop()
+//        {
+//            // 반환할 데이터를 따로 저장
+//            T ret = _heap[0];
 
-            // 마지막 데이터를 루트로 이동
-            int lastIndex = _heap.Count - 1;
-            _heap[0] = _heap[lastIndex];
-            _heap.RemoveAt(lastIndex);
-            lastIndex--;
+//            // 마지막 데이터를 루트로 이동
+//            int lastIndex = _heap.Count - 1;
+//            _heap[0] = _heap[lastIndex];
+//            _heap.RemoveAt(lastIndex);
+//            lastIndex--;
 
-            // 역으로 내려가는 도장깨기 시작
-            int now = 0;
-            while (true)
-            {
-                int left = 2 * now + 1;
-                int right = 2 * now + 2;
+//            // 역으로 내려가는 도장깨기 시작
+//            int now = 0;
+//            while (true)
+//            {
+//                int left = 2 * now + 1;
+//                int right = 2 * now + 2;
 
-                int next = now;
-                // 왼쪽 값이 현재 값보다 크면, 왼쪽 이동
-                if (left <= lastIndex && _heap[next].CompareTo(_heap[left]) < 0)
-                    next = left;
+//                int next = now;
+//                // 왼쪽 값이 현재 값보다 크면, 왼쪽 이동
+//                if (left <= lastIndex && _heap[next].CompareTo(_heap[left]) < 0)
+//                    next = left;
 
-                // 오른값이 현재값보다 크면, 오른쪽으로 이동
-                if (right <= lastIndex && _heap[next].CompareTo(_heap[right]) < 0)
-                    next = right;
+//                // 오른값이 현재값보다 크면, 오른쪽으로 이동
+//                if (right <= lastIndex && _heap[next].CompareTo(_heap[right]) < 0)
+//                    next = right;
 
-                // 왼쪽/오른쪽 모두 현재값보다 작으면 종료
-                if (next == now)
-                    break;
+//                // 왼쪽/오른쪽 모두 현재값보다 작으면 종료
+//                if (next == now)
+//                    break;
 
-                // 두 값 교체
-                T temp = _heap[now];
-                _heap[now] = _heap[next];
-                _heap[next] = temp;
+//                // 두 값 교체
+//                T temp = _heap[now];
+//                _heap[now] = _heap[next];
+//                _heap[next] = temp;
 
-                // 검사 위치를 이동
-                now = next;
-            }
-            return ret;
-        }
+//                // 검사 위치를 이동
+//                now = next;
+//            }
+//            return ret;
+//        }
 
-        public int Count()
-        {
-            return _heap.Count;
-        }
-    }
+//        public int Count()
+//        {
+//            return _heap.Count;
+//        }
+//    }
 
-    class Knight : IComparable<Knight>
-    {
-        public int Id { get; set; }
+//    class Knight : IComparable<Knight>
+//    {
+//        public int Id { get; set; }
 
-        public int CompareTo(Knight other)
-        {
-            if (Id == other.Id)
-                return 0;
-            return Id > other.Id ? 1 : -1;
+//        public int CompareTo(Knight other)
+//        {
+//            if (Id == other.Id)
+//                return 0;
+//            return Id > other.Id ? 1 : -1;
 
-        }
-    }
+//        }
+//    }
 
 
-    internal class Exercise
-    {
+//    internal class Exercise
+//    {
 
-        static void Main(string[] args)
-        {
-            PriorityQueue<Knight> q = new PriorityQueue<Knight>();
-            q.Push(new Knight() { Id = 20 });
-            q.Push(new Knight() { Id = 30 });
-            q.Push(new Knight() { Id = 40 });
-            q.Push(new Knight() { Id = 10 });
-            q.Push(new Knight() { Id = 05 });
-            // 음수를 저장해서 출력할 때 -를 곱해서 출력하면 오름차순 정렬 가능
+//        static void Main(string[] args)
+//        {
+//            PriorityQueue<Knight> q = new PriorityQueue<Knight>();
+//            q.Push(new Knight() { Id = 20 });
+//            q.Push(new Knight() { Id = 30 });
+//            q.Push(new Knight() { Id = 40 });
+//            q.Push(new Knight() { Id = 10 });
+//            q.Push(new Knight() { Id = 05 });
+//            // 음수를 저장해서 출력할 때 -를 곱해서 출력하면 오름차순 정렬 가능
 
-            while (q.Count() > 0)
-            {
-                Console.WriteLine(q.Pop().Id);
-            }
-        }
-    }
-}
+//            while (q.Count() > 0)
+//            {
+//                Console.WriteLine(q.Pop().Id);
+//            }
+//        }
+//    }
+//}
